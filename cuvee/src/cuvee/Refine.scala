850039bb1d1746a1d5b12de5d8348bb18db6b10a
Gidon Ernst <gidonernst@gmail.com>
2019-11-17 18:15:03 +0100

    create diagrams

--- cuvee/src/cuvee/Refine.scala
+++ cuvee/src/cuvee/Refine.scala
@@ -3,7 +3,15 @@ package cuvee
 import java.io.File
 
 case class Step(pre: List[Expr], eqs: List[Eq]) {
+  val free = Set((pre ++ eqs) flatMap (_.free): _*)
 
+  val primed = free collect {
+    case Id(name, index) if name endsWith "1" =>
+      Id(name dropRight 1, index)
+  }
+
+  val in = free filter (_.name startsWith "in")
+  val out = free filter (_.name startsWith "out")
 }
 
 object Step {
@@ -15,17 +23,79 @@ object Step {
   }
 }
 
-case class Trans(init: Step, ops: List[Step]) {
+case class Trans(state: List[Formal], init: Step, ops: List[Step]) {
+  def state1 = state map {
+    case Formal(Id(name, index), typ) =>
+      Formal(Id(name + "1", index), typ)
+  }
 
+  def refine(that: Trans) = {
+    import Trans.diagram
+
+    val as = this.state
+    val as1 = this.state1
+    val cs = that.state // map (_.prime)
+    val cs1 = that.state1
+
+    val R = Id("R")
+    val Rxs = App(R, as ++ cs)
+    val Rxs1 = App(R, as1 ++ cs1)
+
+    val init = diagram(
+      as, as1, this.init,
+      cs, cs1, that.init,
+      True, Rxs)
+
+    val ops = for ((aproc, cproc) <- (this.ops zip that.ops)) yield {
+      diagram(
+        as, as1, aproc,
+        cs, cs1, cproc,
+        Rxs, Rxs1)
+    }
+
+    (Rxs, as, cs, init, ops)
+  }
 }
 
 object Trans {
-  def apply(cmds: List[Cmd]): Trans = {
+  def apply(cmds: List[Cmd], st: State): Trans = {
     val exprs = cmds collect {
       case Assert(expr) => expr
     }
-    val init :: ops = exprs
-    Trans(Step(init), ops map (Step(_)))
+    val steps = exprs map (Step(_))
+    val state = steps flatMap (_.primed) map {
+      id =>
+        val (Nil, typ) = st.funs(id)
+        Formal(id, typ)
+    }
+    val init :: ops = steps
+    Trans(state.distinct, init, ops)
+  }
+
+  def diagram(
+    as: List[Formal], as1: List[Formal], astep: Step,
+    cs: List[Formal], cs1: List[Formal], cstep: Step,
+    R0: Expr, R1: Expr): Expr = {
+
+    val Step(apres, abody) = astep
+    val Step(cpres, cbody) = cstep
+
+    val apre = And(apres)
+    val cpre = And(cpres)
+    val aeq = And(abody)
+    val ceq = And(cbody)
+
+    val ai = astep.in
+    val ao = astep.out
+
+    val ci = cstep.in
+    val co = cstep.out
+
+    val in = (ai & ci).toList
+    val out = (ao & co).toList
+
+    (apre && ceq && R0) ==>
+      (cpre && Exists(as1, aeq && R1))
   }
 }
 
@@ -36,13 +106,33 @@ object Refine {
     val asrc = Source.file(afile)
     val csrc = Source.file(cfile)
 
-    def run(solver: Solver, report: Report) {
+    def run(solver: Solver, report: Report) = solver match {
+      case cuvee: Cuvee => run(cuvee, report)
+      case _ => ???
+    }
+
+    def run(solver: Cuvee, report: Report) {
       val acmds = asrc.cmds
       val ccmds = csrc.cmds
-      val a = Trans(acmds)
-      val c = Trans(ccmds)
-      println(a)
-      println(c)
+
+      val cmds = (acmds ++ ccmds).distinct filterNot (_.isInstanceOf[Assert])
+
+      for (cmd <- cmds) {
+        Source.safe(cmd, solver, report)
+      }
+
+      val a = Trans(acmds, solver.top)
+      val c = Trans(ccmds, solver.top)
+
+      val (rxs, as, cs, init, ops) = a refine c
+
+      val _init = Simplify.simplify(init)
+      println(_init)
+
+      for (op <- ops) {
+        val _op = Simplify.simplify(op)
+        println(_op)
+      }
     }
   }
 
