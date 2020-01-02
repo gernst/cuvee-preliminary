6cb1f0cb9903a6265e9a78779374f407a5328e22
Tillerino <tillmann.gaida@gmail.com>
2020-01-02 18:21:18 +0100

    Test simple verification condition for procedure definition

--- cuvee/test/src/cuvee/CmdTest.scala
+++ cuvee/test/src/cuvee/CmdTest.scala
@@ -8,12 +8,12 @@ object CmdTest extends TestSuite {
       DefineProc(Id("my-proc"), List(Formal(Id("x"), Sort("Int")), Formal(Id("x"), Sort("Int"))), List(), Block(List()), True, True)
       fail("should have thrown an error")
     } catch {
-      case e: Error => assertEquals(e.info.toString, "The method my-proc declares duplicate input parameters x")
+      case e: Error => assertEquals(e.getMessage, "The method my-proc declares duplicate input parameters x")
     }
   }
 
   test("absolute function verification condition") {
     val verificationCondition = ParserTest.abs.verificationCondition(State.default)
-    println(verificationCondition)
+    assertEquals(verificationCondition, Forall(List(Formal(Id("x"), Sort("Int"))), True ==> ((Id("x") < 0 ==> 0 - Id("x") >= 0) && !(Id("x") < 0) ==> Id("x") >= 0)))
   }
 }
