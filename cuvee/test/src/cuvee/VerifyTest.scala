1b9e049b02cfb3e1f3cdd1e02cb35beb620c76cd
Tillerino <tillmann.gaida@gmail.com>
2020-02-25 11:26:06 +0100

    Change gcd-proc into file-based test

--- cuvee/test/src/cuvee/VerifyTest.scala
+++ cuvee/test/src/cuvee/VerifyTest.scala
@@ -116,7 +116,6 @@ object VerifyTest extends TestSuite {
   for (
     file <- List(
       "examples/verifications/abs-proc.smt2",
-      "examples/verifications/gcd-proc.smt2",
       "examples/verifications/zero-proc.smt2",
       "examples/refinements/accounts.smt2")
   ) {
