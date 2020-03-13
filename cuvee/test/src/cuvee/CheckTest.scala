ea0c722a2c68fcbbab99efa6b97bef9b88abc7b7
Gidon Ernst <gidonernst@gmail.com>
2020-03-13 14:54:28 +0100

    simpler lazy values

--- cuvee/test/src/cuvee/CheckTest.scala
+++ cuvee/test/src/cuvee/CheckTest.scala
@@ -8,8 +8,8 @@ import minitest.api.{AssertionException, SourceLocation}
 import scala.collection.immutable.Map.empty
 
 object CheckTest extends TestSuite {
-  val bool: Type = "Bool"
-  val int: Type = "Int"
+  import Sort.bool
+  import Sort.int
 
   // local copies for convenience
   def infer(expr: Expr, ty: Map[Id, Type] = Map.empty, st: State = State.default): Type = Check.infer(expr, ty, st)
@@ -20,7 +20,7 @@ object CheckTest extends TestSuite {
     assertEquals(infer(e"false"), bool)
     assertEquals(infer(e"0"), int)
 
-    assertError(() => infer(e"whatever"), "unknown identifier")
+    assertError(infer(e"whatever"), "unknown identifier")
   }
 
   test("Function applications") {
@@ -28,10 +28,10 @@ object CheckTest extends TestSuite {
     assertEquals(infer(e"(and true false)"), bool)
     assertEquals(infer(e"(or (and true false) true)"), bool)
 
-    assertError(() => infer(e"(whatever 1 true)"), "unknown function")
-    assertError(() => infer(e"(and true 1)"), "signature")
-    assertError(() => infer(e"(or (and true 1) true)"), "signature")
-    assertError(() => infer(e"(or (and true false) 1)"), "signature")
+    assertError(infer(e"(whatever 1 true)"), "unknown function")
+    assertError(infer(e"(and true 1)"), "signature")
+    assertError(infer(e"(or (and true 1) true)"), "signature")
+    assertError(infer(e"(or (and true false) 1)"), "signature")
   }
 
   test("Weakest precondition in bind with procedure call") {
@@ -39,17 +39,17 @@ object CheckTest extends TestSuite {
 
     assertEquals(infer(e"(forall ((x Int) (y Bool)) (wp (call int-bool-proc (x) (y)) (= y true)))", st = st), Sort.bool)
 
-    assertError(() => infer(e"(forall ((x Int) (y Bool)) (wp (call int-bool-proc (x) (z)) (= y true)))", st = st), "unknown identifier")
+    assertError(infer(e"(forall ((x Int) (y Bool)) (wp (call int-bool-proc (x) (z)) (= y true)))", st = st), "unknown identifier")
 
-    assertError(() => infer(e"(forall ((x Int) (y Bool)) (wp (call int-bool-proc (x) (x)) (= y true)))", st = st), "signature")
+    assertError(infer(e"(forall ((x Int) (y Bool)) (wp (call int-bool-proc (x) (x)) (= y true)))", st = st), "signature")
 
-    assertError(() => infer(e"(forall ((x Int) (y Bool)) (wp (call int-bool-proc (y) (y)) (= y true)))", st = st), "signature")
+    assertError(infer(e"(forall ((x Int) (y Bool)) (wp (call int-bool-proc (y) (y)) (= y true)))", st = st), "signature")
 
-    assertError(() => infer(e"(forall ((x Int) (y Bool)) (wp (call int-bool-proc (x) (y)) x))", st = st), "post-condition")
+    assertError(infer(e"(forall ((x Int) (y Bool)) (wp (call int-bool-proc (x) (y)) x))", st = st), "post-condition")
   }
 
   test("Break") {
-    assertError(() => checkProg(p"(block (break))"), "break");
+    assertError(checkProg(p"(block (break))"), "break");
 
     checkProg(p"(while true (break))");
   }
@@ -59,7 +59,7 @@ object CheckTest extends TestSuite {
 
     assertEquals(infer(e"(let ((x y)) (x))", Map(Id("y") -> bool), default), bool)
 
-    assertError(() => infer(e"(let ((x y)) (x))"), "unknown identifier")
+    assertError(infer(e"(let ((x y)) (x))"), "unknown identifier")
   }
 
   test("Spec") {
@@ -67,14 +67,14 @@ object CheckTest extends TestSuite {
 
     checkProg(p"(spec (x) (> x 0) (<= x 0))", xDef)
 
-    assertError(() => checkProg(p"(spec (x) (> x 0) (<= x 0))"), "unknown")
-    assertError(() => checkProg(p"(spec (x) (> x 0) (<= x true))", xDef), "signature")
-    assertError(() => checkProg(p"(spec (x) (> x false) (<= x 0))", xDef), "signature")
+    assertError(checkProg(p"(spec (x) (> x 0) (<= x 0))"), "unknown")
+    assertError(checkProg(p"(spec (x) (> x 0) (<= x true))", xDef), "signature")
+    assertError(checkProg(p"(spec (x) (> x false) (<= x 0))", xDef), "signature")
   }
 
-  private def assertError[T](fn: () => T, message: String)(implicit pos: SourceLocation): Unit = {
+  private def assertError[T](fn: => T, message: String)(implicit pos: SourceLocation): Unit = {
     try {
-      fn()
+      fn
       throw new AssertionException("Expected an error", pos)
     } catch {
       case e: AssertionException => throw e
