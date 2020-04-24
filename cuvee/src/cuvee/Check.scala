560ce218fd43ad97da662f1a3c662ae91baa1549
Tillerino <tillmann.gaida@gmail.com>
2020-04-24 15:37:34 +0200

    Turn check into a case class
    This way we don't have to pass the state through all functions

--- cuvee/src/cuvee/Check.scala
+++ cuvee/src/cuvee/Check.scala
@@ -3,9 +3,9 @@ package cuvee
 import cuvee.Sort.{bool, int}
 import cuvee.Type.array
 
-object Check {
-  def infer(expr: Expr, ty: Map[Id, Type], st: State, implied: Option[Type] = None): Type = try {
-    doInfer(expr, ty, st, implied)
+case class Check(st: State) {
+  def infer(expr: Expr, ty: Map[Id, Type], implied: Option[Type] = None): Type = try {
+    doInfer(expr, ty, implied)
   } catch {
     case SuggestedDeclarationError(suggestion, info) =>
       throw SuggestedDeclarationError(suggestion, info ++ List(expr.toString))
@@ -15,7 +15,7 @@ object Check {
       throw other
   }
 
-  private def doInfer(expr: Expr, ty: Map[Id, Type], st: State, implied: Option[Type] = None): Type = expr match {
+  private def doInfer(expr: Expr, ty: Map[Id, Type], implied: Option[Type] = None): Type = expr match {
     case _: Num =>
       Sort.int
 
@@ -31,10 +31,10 @@ object Check {
       error(s"unknown identifier $expr")
 
     case Note(expr, _) =>
-      infer(expr, ty, st)
+      infer(expr, ty)
 
     case UMinus(arg) =>
-      val typ = infer(arg, ty, st, Some(int))
+      val typ = infer(arg, ty, Some(int))
       ensure(typ == Sort.int, s"argument for unary minus must be integer but was $typ")
       Sort.int
 
@@ -42,56 +42,56 @@ object Check {
       val (ts2, tr) = st funs id
       ensure(args.length == ts2.length, s"wrong number of arguments for $id." +
         s" Expected ${ts2.length} but found ${args.length}.")
-      val ts1 = (args zip ts2) map (x => infer(x._1, ty, st, Some(x._2)))
+      val ts1 = (args zip ts2) map (x => infer(x._1, ty, Some(x._2)))
       ensure(ts1 == ts2, s"arguments for $id do not match function signature." +
         s" Expected ${sig(ts2)} but was ${sig(ts1)}")
       tr
 
     case App(id, args) =>
-      val ts1 = args map (infer(_, ty, st))
+      val ts1 = args map ((expr: _root_.cuvee.Expr) => infer(expr, ty))
       declareFun(id, ts1, implied)
 
     case Bind(_, formals, body) =>
       val ts: List[(Id, Type)] = formals
-      val tr = infer(body, ty ++ ts, st, Some(bool))
+      val tr = infer(body, ty ++ ts, Some(bool))
       ensure(tr == Sort.bool, s"body of bind must be boolean but was $tr")
       tr
 
     case Old(expr) =>
-      infer(expr, ty, st, implied)
+      infer(expr, ty, implied)
 
     case Eq(left, right) =>
-      val (lt, rt) = inferTwoSides(ty, st, left, right)
+      val (lt, rt) = inferTwoSides(ty, left, right)
       ensure(lt == rt, s"types of sides of equals must match but were $lt and $rt")
       Sort.bool
 
     case Distinct(exprs) =>
-      val types = exprs.map(infer(_, ty, st)).distinct
+      val types = exprs.map(infer(_, ty)).distinct
       ensure(types.length == 1, s"types of arguments of distinct must match but were ${types mkString " != "}")
       Sort.bool
 
     case Ite(test, left, right) =>
-      val tt = infer(test, ty, st, Some(bool))
+      val tt = infer(test, ty, Some(bool))
       ensure(tt == Sort.bool, s"test of if-then-else expression must be boolean but was $tt")
-      val (lt, rt) = inferTwoSides(ty, st, left, right)
+      val (lt, rt) = inferTwoSides(ty, left, right)
       ensure(lt == rt, s"types of then- and else-clause must match but were $lt != $rt")
       lt
 
-    case WP(prog, post) => inferWpLike(prog, post, ty, st)
-    case Box(prog, post) => inferWpLike(prog, post, ty, st)
-    case Dia(prog, post) => inferWpLike(prog, post, ty, st)
+    case WP(prog, post) => inferWpLike(prog, post, ty)
+    case Box(prog, post) => inferWpLike(prog, post, ty)
+    case Dia(prog, post) => inferWpLike(prog, post, ty)
 
     case Let(pairs, body) =>
-      val vars = pairs.map(pair => pair.x -> infer(pair.e, ty, st)).toMap
-      infer(body, ty ++ vars, st)
+      val vars = pairs.map(pair => pair.x -> infer(pair.e, ty)).toMap
+      infer(body, ty ++ vars)
 
     case Match(expr, cases) =>
-      infer(expr, ty, st) match {
+      infer(expr, ty) match {
         case sort: Sort =>
           ensure(st.datatypes contains sort, s"trying to match non-datatype $sort")
 
           val types = for (Case(pat, expr) <- cases) yield {
-            inferCase(sort, pat, expr, ty, st)
+            inferCase(sort, pat, expr, ty)
           }
           ensure(types.distinct.size == 1, s"match cases must return equal type but found ${types.distinct.mkString(", ")}")
 
@@ -99,9 +99,9 @@ object Check {
         case typ => error(s"can only match sorts, found $typ")
       }
 
-    case Select(arr, index) => infer(arr, ty, st) match {
+    case Select(arr, index) => infer(arr, ty) match {
       case array(dom, ran) =>
-        val it = infer(index, ty, st, Some(dom))
+        val it = infer(index, ty, Some(dom))
         ensure(it == dom, s"expected index to be of type $dom byt was $it")
 
         ran
@@ -109,12 +109,12 @@ object Check {
         error(s"expected array type but got $other")
     }
 
-    case Store(arr, index, value) => infer(arr, ty, st) match {
+    case Store(arr, index, value) => infer(arr, ty) match {
       case typ @ array(dom, ran) =>
-        val it = infer(index, ty, st, Some(dom))
+        val it = infer(index, ty, Some(dom))
         ensure(it == dom, s"expected index to be of type $dom byt was $it")
 
-        val vt = infer(value, ty, st, Some(ran))
+        val vt = infer(value, ty, Some(ran))
         ensure(vt == ran, s"expected value to be stored in array of type $ran but was $vt")
 
         typ
@@ -126,7 +126,7 @@ object Check {
       (st.objects get a, st.objects get c) match {
         case (Some(ao), Some(co)) =>
           val locals: Map[Id, Type] = ao.state ++ co.state
-          val t = infer(App(r, ao.state ++ co.state), ty ++ locals, st, Some(bool))
+          val t = infer(App(r, ao.state ++ co.state), ty ++ locals, Some(bool))
           ensure(t == bool, s"Refinement $r must be boolean but was $t")
           bool
         case (None, _) =>
@@ -136,46 +136,46 @@ object Check {
       }
   }
 
-  private def inferTwoSides(ty: Map[Id, Type], st: State, left: Expr, right: Expr) = {
+  private def inferTwoSides(ty: Map[Id, Type], left: Expr, right: Expr) = {
     // Either side can imply the type of the other side.
     // If nothing can be implied, we want to see the errors for the left side first.
     // That's why we're checking things in this order.
     try {
-      val rt = infer(right, ty, st)
-      val lt = infer(left, ty, st, Some(rt))
+      val rt = infer(right, ty)
+      val lt = infer(left, ty, Some(rt))
       (lt, rt)
     } catch {
       case e: SuggestedDeclarationError =>
         throw e
       case other: Exception =>
-        val lt = infer(left, ty, st)
-        val rt = infer(right, ty, st, Some(lt))
+        val lt = infer(left, ty)
+        val rt = infer(right, ty, Some(lt))
         (lt, rt) // can't be actually be returned because the type check did once before
     }
   }
 
-  private def inferCase(typ: Type, pat: Pat, expr: Expr, ty: Map[Id, Type], st: State): Type = pat match {
-    case id: Id => infer(expr, ty + (id -> typ), st)
+  private def inferCase(typ: Type, pat: Pat, expr: Expr, ty: Map[Id, Type]): Type = pat match {
+    case id: Id => infer(expr, ty + (id -> typ))
     case UnApp(fun, args) =>
       ensure(st.funs contains fun, s"unknown constructor $fun", pat)
       val (ct, dt) = st.funs(fun)
       ensure(typ == dt, s"trying to match $typ with $dt-constructor $fun", pat)
       ensure(args.length == ct.length, s"wrong number of arguments for $fun", pat)
-      infer(expr, ty ++ args.zip(ct), st)
+      infer(expr, ty ++ args.zip(ct))
   }
 
-  private def inferWpLike(prog: Prog, post: Expr, ty: Map[Id, Type], st: State): Type = {
-    val postt = infer(post, ty, st, Some(bool))
+  private def inferWpLike(prog: Prog, post: Expr, ty: Map[Id, Type]): Type = {
+    val postt = infer(post, ty, Some(bool))
     ensure(postt == Sort.bool, s"post-condition must be boolean but was $postt", post)
-    checkProg(prog, ty, st, loop = false)
+    checkProg(prog, ty, loop = false)
     Sort.bool
   }
 
   /**
    * @param loop true if this program is in a loop. This allows the break statement.
    */
-  def checkProg(prog: Prog, ty: Map[Id, Type], st: State, loop: Boolean): Unit = try {
-    doCheckProg(prog, ty, st, loop)
+  def checkProg(prog: Prog, ty: Map[Id, Type], loop: Boolean): Unit = try {
+    doCheckProg(prog, ty, loop)
   } catch {
     case SuggestedDeclarationError(suggestion, info) =>
       throw SuggestedDeclarationError(suggestion, info ++ List(prog.toString))
@@ -188,10 +188,10 @@ object Check {
   /**
    * @param loop true if this program is in a loop. This allows the break statement.
    */
-  private def doCheckProg(prog: Prog, ty: Map[Id, Type], st: State, loop: Boolean): Unit = prog match {
+  private def doCheckProg(prog: Prog, ty: Map[Id, Type], loop: Boolean): Unit = prog match {
     case Block(progs, _) =>
       for (prog <- progs)
-        checkProg(prog, ty, st, loop)
+        checkProg(prog, ty, loop)
 
     case Break =>
       ensure(loop, "break must only occur in while")
@@ -207,47 +207,47 @@ object Check {
         } else {
           error(s"unknown variable $id")
         }
-        val actual = infer(value, ty, st, Some(expected))
+        val actual = infer(value, ty, Some(expected))
         ensure(expected == actual, s"value of type $actual cannot be assigned to variable $id of type $expected")
       }
 
     case Spec(xs, pre, post) =>
-      xs.foreach(infer(_, ty, st)) // just check if defined
+      xs.foreach(infer(_, ty)) // just check if defined
 
-      val pret = infer(pre, ty, st, Some(bool))
+      val pret = infer(pre, ty, Some(bool))
       ensure(pret == Sort.bool, s"pre-condition must be boolean but was $pret", pre)
 
-      val postt = infer(post, ty, st, Some(bool))
+      val postt = infer(post, ty, Some(bool))
       ensure(postt == Sort.bool, s"post-condition must be boolean but was $postt", post)
 
     case If(test, left, right) =>
-      val tt = infer(test, ty, st, Some(bool))
+      val tt = infer(test, ty, Some(bool))
       ensure(tt == Sort.bool, s"test of if-then-else statement must be boolean but was $tt", test)
-      checkProg(left, ty, st, loop)
-      checkProg(right, ty, st, loop)
+      checkProg(left, ty, loop)
+      checkProg(right, ty, loop)
 
     case While(test, body, after, term, pre, post) =>
-      val tt = infer(test, ty, st, Some(bool))
+      val tt = infer(test, ty, Some(bool))
       ensure(tt == Sort.bool, s"test of while statement must be boolean but was $tt", test)
 
-      checkProg(body, ty, st, loop = true)
-      checkProg(after, ty, st, loop)
+      checkProg(body, ty, loop = true)
+      checkProg(after, ty, loop)
 
-      val termt = infer(term, ty, st, Some(int))
+      val termt = infer(term, ty, Some(int))
       ensure(termt == Sort.int, s"termination expression must be integral but was $termt", term)
 
-      val pret = infer(pre, ty, st, Some(bool))
+      val pret = infer(pre, ty, Some(bool))
       ensure(pret == Sort.bool, s"pre-condition must be boolean but was $pret", pre)
 
-      val postt = infer(post, ty, st, Some(bool))
+      val postt = infer(post, ty, Some(bool))
       ensure(postt == Sort.bool, s"post-condition must be boolean but was $postt", post)
 
     case Call(name, in, out) if st.procs contains name =>
       val (xs, ys) = st.procs(name)
       ensure(in.length == xs.length, s"wrong number of arguments in procedure call to $name. " +
         s"Expected ${xs.length} but got ${in.length}.")
-      val args = (in zip xs) map (p => infer(p._1, ty, st, Some(p._2)))
-      val ass = out map (infer(_, ty, st))
+      val args = (in zip xs) map (p => infer(p._1, ty, Some(p._2)))
+      val ass = out map (infer(_, ty))
       ensure(xs == args, s"procedure call arguments do not math function signature. Expected ${sig(xs)} but was ${sig(args)}")
       ensure(ys == ass, s"procedure return values do not math function signature. Expected ${sig(ys)} but was ${sig(ass)}")
 
@@ -258,15 +258,15 @@ object Check {
       for (x <- xs) {
         ensure(ty contains x, s"unknown variable $x")
       }
-      val typ = infer(phi, ty, st, Some(bool))
+      val typ = infer(phi, ty, Some(bool))
       ensure(typ == Sort.bool, s"condition of choose must be boolean but was $typ", phi)
   }
 
-  def checkObj(sort: Sort, obj: Obj, st: State): Unit = {
+  def checkObj(sort: Sort, obj: Obj): Unit = {
     val Obj(xs, init, ops) = obj
-    obj.state.foreach(f => checkType(f.typ, st))
-    Check.checkProc(Id.init, init, st, xs)
-    ops.foreach(proc => Check.checkProc(proc._1, proc._2, st, xs))
+    obj.state.foreach(f => checkType(f.typ))
+    checkProc(Id.init, init, xs)
+    ops.foreach(proc => checkProc(proc._1, proc._2, xs))
   }
 
   /**
@@ -276,9 +276,9 @@ object Check {
    * @param id name of the procedure for error messages
    * @param xs (optional) state of the surrounding object if this procedure is defined on an object
    */
-  def checkProc(id: Id, proc: Proc, st: State, xs: List[Formal] = Nil): Unit = {
+  def checkProc(id: Id, proc: Proc, xs: List[Formal] = Nil): Unit = {
     val Proc(in, out, pre, post, body) = proc
-    (in ++ out).types.foreach(Check.checkType(_, st))
+    (in ++ out).types.foreach(checkType(_))
     val inVars: List[Id] = in
     val duplicateInputDeclarations = inVars.groupBy(identity).filter(_._2.size > 1)
     ensure(duplicateInputDeclarations.isEmpty, s"The method $id declares duplicate input parameters ${duplicateInputDeclarations.keys.mkString(", ")}")
@@ -289,14 +289,14 @@ object Check {
     ensure(nonUniqueAgruments.isEmpty, "The method $id declares non-unique type for argument ${nonUniqueAgruments.keys.mkString(", ")}")
 
     for (body <- body) {
-      checkBody(id, body, in, out, st, xs)
+      checkBody(id, body, in, out, xs)
     }
 
-    ensure(infer(pre, xs ++ in, st, Some(bool)) == bool, "precondition must be boolean")
-    ensure(infer(post, xs ++ in ++ out, st, Some(bool)) == bool, "postcondition must be boolean")
+    ensure(infer(pre, xs ++ in, Some(bool)) == bool, "precondition must be boolean")
+    ensure(infer(post, xs ++ in ++ out, Some(bool)) == bool, "postcondition must be boolean")
   }
 
-  def checkBody(id: Id, body: Body, in: List[Formal], out: List[Formal], st: State, xs: List[Formal] = Nil): Unit = {
+  def checkBody(id: Id, body: Body, in: List[Formal], out: List[Formal], xs: List[Formal] = Nil): Unit = {
     val inVars: List[Id] = in
 
     // procedure must at most modify its own variables
@@ -310,7 +310,7 @@ object Check {
     val unsetOutputs = outputsThatMustBeSet.filter(!modifiedVariables.contains(_))
     ensure(unsetOutputs.isEmpty, s"The method $id does not modify its output parameters ${unsetOutputs.mkString(", ")}")
 
-    checkProg(body.prog, in ++ out ++ body.locals ++ xs, st, loop = false)
+    checkProg(body.prog, in ++ out ++ body.locals ++ xs, loop = false)
   }
 
   private def sig(ts2: List[Type]) = {
@@ -331,7 +331,7 @@ object Check {
     }
   }
 
-  def checkType(typ: Type, st: State): Unit = {
+  def checkType(typ: Type): Unit = {
     typ match {
       case s: Sort if !(st.sorts contains s) =>
         val decl = s"(declare-sort $s)"
@@ -341,10 +341,10 @@ object Check {
           d.declare(s, 0)
         }, Seq(msg))
       case Type.list(elem) =>
-        checkType(elem, st)
+        checkType(elem)
       case array(dom, ran) =>
-        checkType(dom, st)
-        checkType(ran, st)
+        checkType(dom)
+        checkType(ran)
       case _ => // we're Gucci
     }
   }
--- cuvee/test/src/cuvee/VerifyTest.scala
+++ cuvee/test/src/cuvee/VerifyTest.scala
@@ -7,7 +7,7 @@ import cuvee.testutils.Implicits._
 object VerifyTest extends TestSuite {
   test("cannot declare duplicate procedure parameters") {
     try {
-      Check.checkProc("my-proc", Proc(List(Formal(Id("x"), Sort("Int")), Formal(Id("x"), Sort("Int"))), List(), True, True), State.default)
+      Check(State.default).checkProc("my-proc", Proc(List(Formal(Id("x"), Sort("Int")), Formal(Id("x"), Sort("Int"))), List(), True, True))
       fail("should have thrown an error")
     } catch {
       case e: Error => assertEquals(e.getMessage, "The method my-proc declares duplicate input parameters x")
